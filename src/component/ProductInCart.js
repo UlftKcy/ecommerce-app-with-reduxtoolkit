1e896c3e544a544ccf5093bd95e0140900bcabdb
ulfet <ukacay@albasoft.com.tr>
2022-07-08 11:16:19 +0300

    added card to cart

--- src/component/ProductInCart.js
+++ src/component/ProductInCart.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { FaPlus, FaMinus,FaTrash } from "react-icons/fa";
 
-const ProductInCart = ({product}) => {
+const ProductInCartWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
+  align-items: center;
+  gap: 5px;
+  padding: 1rem;
+  border: 1px solid #d6d6d6;
+  border-radius:25px;
+  margin-bottom:1rem;
+`;
+
+const ProductImage = styled.img`
+  width: 70px;
+  height: 70px;
+  margin: auto;
+`;
+const ProductName = styled.h4``;
+const ProductCount = styled.div``;
+const ProductPrice = styled.span``;
+const ProductCountDecrease = styled.button`
+  padding: 5px 10px;
+  margin: 0 10px;
+  border: 0;
+  cursor: pointer;
+  background-color: #ff7d1a;
+  color: #ffff;
+  border-radius: 5px;
+  &:hover {
+    opacity: 0.9;
+  }
+`;
+const ProductCountIncrease = styled.button`
+  padding: 5px 10px;
+  margin: 0 10px;
+  border: 0;
+  cursor: pointer;
+  background-color: #ff7d1a;
+  color: #ffff;
+  border-radius: 5px;
+  &:hover {
+    opacity: 0.9;
+  }
+`;
+const RemoveProductButton = styled.button`
+border:0;
+cursor: pointer;
+color:red;
+`;
+
+const ProductInCart = ({ product }) => {
+  console.log(product);
   return (
-    <div>{product.name}</div>
-  )
-}
+    <ProductInCartWrapper>
+      <ProductImage src={product.image}></ProductImage>
+      <ProductName>{product.name}</ProductName>
+      <ProductCount>
+        <ProductCountDecrease>
+          <FaMinus />
+        </ProductCountDecrease>
+        1
+        <ProductCountIncrease>
+          <FaPlus />
+        </ProductCountIncrease>
+      </ProductCount>
+      <ProductPrice>${product.price}</ProductPrice>
+      <RemoveProductButton><FaTrash size={25}/></RemoveProductButton>
+    </ProductInCartWrapper>
+  );
+};
 
-export default ProductInCart
+export default ProductInCart;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React from "react";
 import { useSelector } from "react-redux";
-import ProductInCart from '../component/ProductInCart';
+import styled from "styled-components";
+import ProductInCart from "../component/ProductInCart";
+
+const CartWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  grid-template-rows: 1fr;
+  gap: 1rem;
+  padding:1rem;
+  
+`;
+const ShoppingCart = styled.div`
+  padding:0 3rem;
+`;
+const Checkout = styled.div`
+ 
+`;
+const ShoppingCartHeader = styled.h3`
+margin-bottom:1rem;
+`
+const CheckoutHeader = styled.h3`
+margin-bottom:1rem;
+`
 
 const Cart = () => {
-  const products = useSelector((state)=>state.cart.total_products_in_cart);
-  const product_count = useSelector((state)=>state.cart.total_count_products_in_cart);
-  console.log("products",products,"product_count",product_count)
+  const products = useSelector((state) => state.cart.total_products_in_cart);
+  const product_count = useSelector(
+    (state) => state.cart.total_count_products_in_cart
+  );
+  console.log("products", products, "product_count", product_count);
   return (
-    <div>
-      {products.map((product)=>(
-        <ProductInCart product={product} key={product.id}/>
+    <CartWrapper>
+      <ShoppingCart>
+        <ShoppingCartHeader>Shopping Cart</ShoppingCartHeader>
+        {products.map((product) => (
+        <ProductInCart product={product} key={product.id} />
       ))}
-    </div>
-  )
-}
+      </ShoppingCart>
+      <Checkout><CheckoutHeader>Checkout</CheckoutHeader></Checkout>
+    </CartWrapper>
+  );
+};
 
 export default Cart;
